88c06ed72f32956521e41b551bc1af16b946234a
TastefullElm548 <92990534+TastefullElm548@users.noreply.github.com>
2023-05-18 16:22:39 +0200

    Update script.js

--- error/script.js
+++ error/script.js
@@ -1,3 +1,3 @@
 let link = document.getElementById("link");
 link.textContent = `masos.my.to/${window.location.search.substr(1)}`;
-link.setAttribute("href", `masos.my.to/${window.location.search.substr(1)}`);
+link.setAttribute("href", `https://masos.my.to/${window.location.search.substr(1)}`);
